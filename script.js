f8a0b3988201c1621593eff7033ff6c2a97576a3
khanh.le1 <khanh.le1@mservice.com.vn>
2022-07-20 15:53:04 +0700

    a

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const data = "Save data to clipboard successfully."
 navigator.clipboard.writeText(data)
+
+const btn = document.getElementById("copy")
+
+btn.addEventListener("click", () => navigator.clipboard.writeText(data))
